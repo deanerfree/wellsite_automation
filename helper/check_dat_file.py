6724a155f47c675bc177ccf4fd721797dad84494
deanerfree <kurtisdball@hotmail.com>
2022-08-23 22:02:15 -0600

    check data, additional data

---
+++ helper/check_dat_file.py
@@ -0,0 +1,56 @@
+import csv
+
+# data_file = "input_files/rawData.dat"
+
+def sum_porosity(por_values, well_data, value, count, start_point, end_point):
+  for well in well_data:
+    if(len(well) >= 4 and well[2] == value):
+
+          if(float(well[0].replace(" ","")) >= float(start_point) and float(well[4]) <= float(end_point)):
+            count += float(well[4])-float(well[0].replace(" ",""))
+            if(well[5] == '0' or well[5] == '1'):
+              por_values['por0'] = float(well[4])-float(well[0].replace(" ",""))+por_values['por0']
+            if(well[5] == '2'):
+              por_values['por1'] = float(well[4])-float(well[0].replace(" ",""))+por_values["por1"]
+            if(well[5] == '3'):
+              por_values['por2'] = float(well[4])-float(well[0].replace(" ",""))+por_values["por2"]
+            if(well[5] == '4'):
+              por_values['por3'] = float(well[4])-float(well[0].replace(" ",""))+por_values["por3"]
+            if(well[5] == '5'):
+              por_values['por4'] = float(well[4])-float(well[0].replace(" ",""))+por_values["por4"]
+            if(well[5] == '6' or well[5] == '7'):
+              por_values['por5'] = float(well[4])-float(well[0].replace(" ",""))+por_values["por5"]
+
+    else:
+      continue
+  sumtotal = por_values['por0']+por_values['por1']+por_values['por2']+por_values['por3']+por_values['por4']+por_values['por5']
+  print(sumtotal)
+  return por_values
+
+def set_porosity (data_file, start_point, end_point):
+  target_value = '4407'
+  count = 0 
+  por_struct={"por0": 0, "por1": 0,"por2": 0, "por3": 0,"por4": 0, "por5": 0}
+ 
+  with open(data_file, 'r', encoding="ISO-8859-1") as well_data:
+    well_data_reader = csv.reader(well_data, delimiter='\t')
+    return sum_porosity(por_struct, well_data_reader, target_value, count, start_point, end_point)
+    
+
+
+def check_data_exists(leg_update, data_file):
+  cleanedupdata = leg_update.replace(" ", "").lower()
+  target_value="SOURCE"
+  with open(data_file, 'r', encoding="ISO-8859-1") as well_data:
+    well_data_reader = csv.reader(well_data, delimiter='\t')
+
+    for well in well_data_reader:
+      if len(well) >= 4 and well[1] == target_value:
+        file_name = well[4].replace(" ","")[:-4].lower()
+        target = file_name.find(cleanedupdata)
+        if target:
+          return True
+        else:
+          return False
+        
+
--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-from helper.porosity import set_porosity
+from helper.check_dat_file import set_porosity
 from helper.edit_geoforms import edit_geo_forms
 from helper.set_intrvls import set_intrvls
+import chardet
 
 # 4407 unique identifier for porosity
 
 data_file = "input_files/rawData.dat"
+data_file2 = "input_files/raw_dat2.dat"
 
 porosity_ss = "input_files/WL_Geoforms.xlsm"
 # data =  {'por0': 92.0, 'por1': 88.5, 'por2': 371.5, 'por3': 628.0, 'por4': 305.0, 'por5': 8.5}
@@ -13,9 +15,15 @@ output_file = "output_files/demo.xlsm"
 start_message = "Enter the start depth: "
 end_message = "Enter the end depth: "
 
+def check(data):
+  rawdata = open(data, 'rb').read()
+  result = chardet.detect(rawdata)
+  charenc = result['encoding']
+  print(charenc)
 
+check(data_file2)
 start_point = set_intrvls(start_message)
 end_point = set_intrvls(end_message)
    
-porosity_values = set_porosity(data_file, start_point, end_point)
+porosity_values = set_porosity(data_file2, start_point, end_point)
 edit_geo_forms(porosity_ss, porosity_values, output_file)
